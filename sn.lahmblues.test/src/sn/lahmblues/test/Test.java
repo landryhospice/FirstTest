bca7555e157ec6a09c2a878ad1f17384f9999bb7
landryhospice <landry.hospice@gmail.com>
2013-04-17 06:29:39 +0100

    Troisiemme commit

--- sn.lahmblues.test/src/sn/lahmblues/test/Test.java
+++ sn.lahmblues.test/src/sn/lahmblues/test/Test.java
@@ -3,7 +3,7 @@ package sn.lahmblues.test;
 public class Test {
 	
 	public static void main (String [] args) {
-	    System.out.println ( "Git pas mal" );
+	    System.out.println ( "Git gestion des versions" );
 	  }
 
 }
