e57c2156828ee189f0cc3eea840fdf04400ee35c
landryhospice <landry.hospice@gmail.com>
2013-04-17 06:18:24 +0100

    deuxieme commit

--- sn.lahmblues.test/src/sn/lahmblues/test/Test.java
+++ sn.lahmblues.test/src/sn/lahmblues/test/Test.java
@@ -3,7 +3,7 @@ package sn.lahmblues.test;
 public class Test {
 	
 	public static void main (String [] args) {
-	    System.out.println ( "Git est un plaisir" );
+	    System.out.println ( "Git pas mal" );
 	  }
 
 }
